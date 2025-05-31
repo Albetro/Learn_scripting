46229d91860ce17ced2f943f3ef8d00a55f1a0fb
Albetro <albertxang@gmail.com>
2025-05-31 23:58:09 +0300

    for loop ping

--- starter/ping_domain.sh
+++ starter/ping_domain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for x in google.com bing.com facebook.com youtube.com;
+for x in google.com bing.com facebook.com youtube.com networkchuck.com;
 do
   if ping -q -c 2 -w 1 $x > /dev/null; then 
      echo "$x is up"
